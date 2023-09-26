cefb070a09dbccbd23c24fd03e4f5fa3fec9c293
adaolena <adrien.dao-lena@altirnao.com>
2023-09-26 15:37:17 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
-import { setFailed } from "@actions/core";
+import {  setFailed, warning } from "@actions/core";
 import { getConfig } from "./get-config";
 import { mergeOrPr } from "./merge-or-pr";
 
 async function run() {
   try {
+    warning('create pr')
+
     const config = getConfig();
     await mergeOrPr(config);
   } catch (error) {
